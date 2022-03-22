2a2fea5d9b1914108333d4c03a3cbfa89fe80b86
PantheraDigital <pantheradigitalonline@gmail.com>
2022-03-22 12:46:43 -0700

    seperated vector class

--- CSharpConsoleApp1/programfiles/Program.cs
+++ CSharpConsoleApp1/programfiles/Program.cs
@@ -9,25 +9,7 @@ using FastConsole;
 
 namespace AsciiProgram
 {
-    public struct Vector2
-    {
-        public int x;
-        public int y;
-
-        public Vector2(int xPos, int yPos)
-        {
-            x = xPos;
-            y = yPos;
-        }
-
-        public bool IsEqual(Vector2 other)
-        {
-            if (other.x == x && other.y == y)
-                return true;
-            else
-                return false;
-        }
-    }
+    
 
     public class Tile
     {
