76a21fb68d4e07d221f8cbca90852d8e0c188384
PantheraDigital <pantheradigitalonline@gmail.com>
2022-04-04 21:57:10 -0700

    fileSorting

--- CSharpConsoleApp1/programfiles/gameObjects/GameObject.cs
+++ CSharpConsoleApp1/programfiles/gameObjects/GameObject.cs
@@ -25,36 +25,5 @@ namespace AsciiProgram
 
     }
 
-    public class NoteObject : GameObject
-    {
-        GameWindow m_window;
-        bool m_active;
-
-        public NoteObject(DisplayObject display, GameWindow gameWindow)
-            :base(display, false)
-        {
-            m_window = gameWindow;
-        }
-
-        public override void OnCollide(MovingEntity other)
-        {
-            m_active = true;
-
-            m_window.Draw(4);
-        }
-
-        public override void EndCollide()
-        {
-            m_active = false;
-
-            m_window.Erase();
-        }
-
-        public override void OnDraw()
-        {
-            m_window.Draw(4);
-            Console.ReadKey(true);
-            m_window.Erase();
-        }
-    }
+    
 }
---
+++ CSharpConsoleApp1/programfiles/gameObjects/NoteObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiProgram
+{
+    public class NoteObject : GameObject
+    {
+        GameWindow m_window;
+        bool m_active;
+
+        public NoteObject(DisplayObject display, GameWindow gameWindow)
+            : base(display, false)
+        {
+            m_window = gameWindow;
+        }
+
+        public override void OnCollide(MovingEntity other)
+        {
+            m_active = true;
+
+            m_window.Draw(4);
+        }
+
+        public override void EndCollide()
+        {
+            m_active = false;
+
+            m_window.Erase();
+        }
+
+        public override void OnDraw()
+        {
+            m_window.Draw(4);
+            Console.ReadKey(true);
+            m_window.Erase();
+        }
+    }
+}
