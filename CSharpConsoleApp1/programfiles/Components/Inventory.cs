f4c2fd88685f758314db83e6c914448a8c23b36f
PantheraDigital <pantheradigitalonline@gmail.com>
2022-04-14 14:57:03 -0700

    file seperation after tests

---
+++ CSharpConsoleApp1/programfiles/Components/Inventory.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiProgram
+{
+    public class Inventory : Component
+    {
+        //FastConsole.FastWrite fastWrite = FastConsole.FastWrite.GetInstance();
+        List<GameObject> m_gameObjects;
+        GameWindow m_display;
+        int m_sizeLimit;
+
+        public Inventory(GameWindow gameWindow, int sizeLimit = 0)
+            : base("Inventory")
+        {
+            m_gameObjects = new List<GameObject>();
+            m_display = gameWindow;
+            m_display.SetMessage("");
+            m_sizeLimit = sizeLimit;
+        }
+
+        public override void Draw()
+        {
+            if (m_display != null)
+            {
+                m_display.Draw();
+            }
+
+            /*
+            Vector2 pos = m_display.GetScreenPosition();
+            fastWrite.SetCursorPosition(pos);
+            if(m_gameObjects.Count == 0)
+            {
+                fastWrite.AddToBuffer("test", "empty");
+            }
+            else
+            {
+                fastWrite.ClearLayer("test");
+                foreach (GameObject obj in m_gameObjects)
+                {
+                    fastWrite.AddToBuffer("test", obj.m_displayObject.m_spriteChar.ToString());
+                }
+            }*/
+
+        }
+
+        public bool Add(GameObject gameObject)
+        {
+            if (m_sizeLimit > 0 && m_gameObjects.Count >= m_sizeLimit)
+                return false;
+
+            if (gameObject != null)
+            {
+                m_gameObjects.Add(gameObject);
+                m_display.AddToMessage(gameObject.m_displayObject.m_spriteChar.ToString());
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<GameObject> GetObjects()
+        {
+            return m_gameObjects;
+        }
+
+        public bool HasObjectWithTag(string tag)
+        {
+            foreach (GameObject obj in m_gameObjects)
+            {
+                if (obj.m_tags.Contains(tag))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public GameObject GetObjectWithTag(string tag)
+        {
+            for (int i = 0; i < m_gameObjects.Count; ++i)
+            {
+                if (m_gameObjects[i].m_tags.Contains(tag))
+                {
+                    GameObject temp = m_gameObjects[i];
+                    m_display.RemoveFromMessage(temp.m_displayObject.m_spriteChar.ToString());
+                    m_gameObjects.Remove(m_gameObjects[i]);
+                    return temp;
+                }
+            }
+
+            return null;
+        }
+
+        public bool HasObject(string name)
+        {
+            foreach (GameObject obj in m_gameObjects)
+            {
+                if (obj.m_name.Equals(name))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+
+}
---
+++ CSharpConsoleApp1/programfiles/Entities/ComplexEntity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiProgram
+{
+
+    public class ComplexEntity : MovingEntity, IComponentManager
+    {
+        Dictionary<string, Component> m_components;
+
+        public ComplexEntity(DisplayObject displayObject, Controller controller, string tags = "none")
+            : base(displayObject, controller, tags)
+        {
+            m_components = new Dictionary<string, Component>();
+        }
+
+        public bool HasComponent(string type)
+        {
+            if (m_components.ContainsKey(type))
+                return true;
+            else
+                return false;
+        }
+
+        public T GetComponent<T>(string type)
+        {
+            if (m_components.ContainsKey(type))
+                return (T)Convert.ChangeType(m_components[type], typeof(T));
+            else
+                return default(T);
+        }
+
+        public bool AddComponent(Component component)
+        {
+            if (m_components.ContainsKey(component.GetComponentType()))
+                return false;
+            else
+            {
+                m_components.Add(component.GetComponentType(), component);
+                return true;
+            }
+        }
+        public void DrawComponents()
+        {
+            foreach (string key in m_components.Keys)
+            {
+                m_components[key].Draw();
+            }
+        }
+    }
+
+}
